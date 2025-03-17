9f1457eaf176f5c9411a3382266b2ab3794c8ab1
Manuel-Martinez-324 <130503508+Manuel-Martinez-324@users.noreply.github.com>
2025-03-17 15:41:29 -0600

    Add files via upload

---
+++ challenge-amigo-secreto/app.js
@@ -0,0 +1,40 @@
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+function agregarAmigo() {
+    const input = document.getElementById("amigo");
+    const nombre = input.value.trim();
+
+    if (nombre === "") {
+        alert("Por favor, ingresa un nombre válido.");
+        return;
+    }
+
+    amigos.push(nombre);
+    actualizarLista();
+    input.value = "";
+}
+
+function actualizarLista() {
+    const lista = document.getElementById("listaAmigos");
+    lista.innerHTML = "";
+    
+    amigos.forEach((amigo, index) => {
+        const li = document.createElement("li");
+        li.textContent = amigo;
+        lista.appendChild(li);
+    });
+}
+
+function sortearAmigo() {
+    if (amigos.length === 0) {
+        alert("No hay nombres en la lista para sortear.");
+        return;
+    }
+    
+    const indiceSorteado = Math.floor(Math.random() * amigos.length);
+    const amigoSorteado = amigos[indiceSorteado];
+    
+    const resultado = document.getElementById("resultado");
+    resultado.innerHTML = `<li> ${amigoSorteado} es el amigo secreto! </li>`;
+}
